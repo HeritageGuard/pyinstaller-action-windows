2441f5e2bd79bbdf95f8fd4ae498eaa9dcaa105a
Domas <domas.briedis@gmail.com>
2021-03-28 12:45:39 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -41,6 +41,8 @@ fi
 
 cd $WORKDIR
 
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O ./miniconda.sh
+bash ./miniconda.sh -b -p
 conda install -c conda-forge implicit
 pip install -U setuptools
 
