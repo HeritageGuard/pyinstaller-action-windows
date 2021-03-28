9fce4306a23a9a56bc855d511d96a3e44d5cd0ad
Domas <domas.briedis@gmail.com>
2021-03-28 13:26:23 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,7 +20,7 @@ WORKDIR=${SRCDIR:-/src}
 SPEC_FILE=${4:-*.spec}
 
 python -m pip install --upgrade pip wheel setuptools
-curl -LO http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
+wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh
 bash Miniconda3-latest-Linux-x86_64.sh -p /miniconda -b
 rm Miniconda3-latest-Linux-x86_64.sh
 PATH=/miniconda/bin:${PATH}
