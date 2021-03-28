577cc7f5f12d0adc8b69729ac080326f2b2b595b
Domas <domas.briedis@gmail.com>
2021-03-28 19:16:12 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -48,6 +48,8 @@ if [[ "$PYPI_URL" != "https://pypi.python.org/" ]] || \
 fi
 
 cd $WORKDIR
+winetricks dotnet462
+
 wget https://download.visualstudio.microsoft.com/download/pr/1192d0de-5c6d-4274-b64d-c387185e4f45/605089bf72da4da4d27eb0cfcec569ed61f5cf5671aa6d3dece1487abfd62cab/vs_BuildTools.exe
 
 wine cmd.exe /C "vs_BuildTools.exe --quiet --passive --add Microsoft.VisualStudio.Workload.VCTools"
